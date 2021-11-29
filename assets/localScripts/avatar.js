e62f213afcd0999f197dc25706d6971c313f54fa
Antoine Webanck <antoine.webanck@gmail.com>
2021-11-30 00:00:23 +0100

    Working draft!

--- assets/localScripts/avatar.js
+++ assets/localScripts/avatar.js
@@ -1,5 +1,7 @@
 /** @format */
 
+//import * as itowns from 'itowns';
+
 let udviz;
 let Shared = null;
 
@@ -10,7 +12,7 @@ module.exports = class LocalAvatar {
     Shared = udviz.Game.Shared;
 
     this.avatar = null;
-    this.cube = null;
+    this.localAvatar = null;
     this.intersectionCube = null;
     this.inputManager = null;
 
@@ -20,9 +22,9 @@ module.exports = class LocalAvatar {
 
   addObjectToArray(array, tilesManager, layerName) {
     let layerManager = null;
+    //console.log(tilesManager);
     for (let index = 0; index < tilesManager.length; index++) {
       const element = tilesManager[index];
-      //debugger
       if (element.layer.id == layerName) {
         layerManager = element;
         break;
@@ -41,7 +43,7 @@ module.exports = class LocalAvatar {
 
     // const tilesManager = gV.getLayerManager().tilesManagers;
     const buildings = [];
-    this.addObjectToArray(buildings, tilesManager, '3d-tiles-layer-building');
+    //this.addObjectToArray(buildings, tilesManager, '3d-tiles-layer-building');
 
     // const pos = this.avatar.getPosition();
     // const ref = localContext.getGameView().getObject3D().position;
@@ -72,8 +74,8 @@ module.exports = class LocalAvatar {
   }
   groundElevationDelta(tilesManager, origin) {
     const ground = [];
-    this.addObjectToArray(ground, tilesManager, '3d-tiles-layer-relief');
-    this.addObjectToArray(ground, tilesManager, '3d-tiles-layer-road');
+    //this.addObjectToArray(ground, tilesManager, '3d-tiles-layer-relief');
+    //this.addObjectToArray(ground, tilesManager, '3d-tiles-layer-road');
 
     const zShift = 500;
     this.raycaster.ray.origin.set(origin.x, origin.y, origin.z + zShift);
@@ -98,17 +100,16 @@ module.exports = class LocalAvatar {
 
 
 
-
     //local avatar/cube
     const scene = gV.getScene();
-    console.log(avatar.object3D);
+    //console.log(avatar.object3D);
     // const geometry = new Shared.THREE.BoxGeometry(3, 3, 3);
     // const material = new Shared.THREE.MeshBasicMaterial( {color: 0xff0000} );
     // const cube = new Shared.THREE.Mesh( geometry, material );
-    const cube = avatar.object3D;//.clone();
+    const localAvatar = avatar.object3D;//.clone();
     //avatar.object3D.visible = false;
-    this.cube = cube;
-    //scene.add(cube);
+    this.localAvatar = localAvatar;
+    //scene.add(localAvatar);
     //console.log(scene);
 
     //intersection cube
@@ -117,11 +118,100 @@ module.exports = class LocalAvatar {
     this.intersectionCube = new Shared.THREE.Mesh( geometry, material );
     scene.add( this.intersectionCube );
 
+    console.log(scene);
+
+    //*
+    //referential debug gizmo
+    const sphere = new Shared.THREE.SphereGeometry( 1, 32, 16 );
+    const red = new Shared.THREE.MeshBasicMaterial( {color: 0xff0000} );
+    const green = new Shared.THREE.MeshBasicMaterial( {color: 0x00ff00} );
+    const blue = new Shared.THREE.MeshBasicMaterial( {color: 0x0000ff} );
+    //
+    const redSphere = new Shared.THREE.Mesh( sphere, red );
+    const greenSphere = new Shared.THREE.Mesh( sphere, green );
+    const blueSphere = new Shared.THREE.Mesh( sphere, blue );
+    //
+    redSphere.position.setX(5.);
+    greenSphere.position.setY(5.);
+    blueSphere.position.setZ(5.);
+    //
+    redSphere.position.add(worldOrigin);
+    greenSphere.position.add(worldOrigin);
+    blueSphere.position.add(worldOrigin);
+    //
+    scene.add( redSphere );
+    scene.add( greenSphere );
+    scene.add( blueSphere );
+        
+
+
+
+
+
+
+
+
+    const campus = gV.assetsManager.createModel('campus_Yael');
+    //campus.rotateOnAxis(new Shared.THREE.Vector3(0, 0, 1), -1*Math.PI);
+    campus.rotateOnAxis(new Shared.THREE.Vector3(0, 0, 1), -0.5*Math.PI);
+    //campus.position.set(
+    //  1849073.9812757724,
+    //  5170899.853254461,
+    //  189.97654942595022
+    //);
+    console.log("campus", campus);
+    scene.add(campus);
+    //const campusY = arguments[0].computeRoot().findByName('campusY');
+    //campusY.move(-gV.getObject3D().position);
+    //*/
+
+
+    //Campus
+    //const campusY = new Shared.GameObject({
+    //  name: 'campusY',
+    //  components: {
+    //    Render: { idModel: 'campus_Yael' },
+    //    LocalScript: {
+    //      idScripts: [],
+    //    },
+    //  },
+    //});
+    //campusY.object3D.rotateOnAxis(new Shared.THREE.Vector3(0, 0, 1), -1*Math.PI);
+    //scene.add(campusY.object3D);
+
+
+    //Not working test done with CCO
+    /*
+    // Create a GeometryLayer
+    const campusY = new Shared.GameObject({
+      name: 'campusY',
+      components: {
+        Render: { idModel: 'campus_Yael' },
+        LocalScript: {
+          idScripts: [],
+        },
+      },
+    });
+    campusY.object3D.scale.multiply(new Shared.THREE.Vector3(-1, -1, 1));
+    campusY.setOutdated(true);
+    //const source = new udviz.itowns.FileSource({
+    //  crs: 'EPSG:4326',
+    //  features: campusY.object3D
+    //});
+    const source = new itowns.FileSource({
+      url: "./assets/models/BRON_Nov_15.glb",
+      crs: 'EPSG:4326',
+      fetcher: itowns.Fetcher.xml,
+      parser: itowns.KMLParser.parse,
+    });
+    const layer = new udviz.itowns.GeometryLayer('campus_layer', new Shared.THREE.Object3D(), { source: source });
+    gV.itownsView.addLayer(layer);
+    //*/
 
     const dt = localContext.getDt();
-    const translationSpeed = 0.03;
+    const translationSpeed = 0.05;
     const translationLength = translationSpeed * dt;
-    const speedRotate = 0.0006;
+    const speedRotate = 0.0012;
 
     const checkCollisionFun = function(direction) {
       const origin = avatar.getPosition().clone().add(worldOrigin);
@@ -133,6 +223,7 @@ module.exports = class LocalAvatar {
         this.intersectionCube.visibility = true;
         this.intersectionCube.position.copy(intersection.point);
         this.intersectionCube.updateMatrixWorld();
+        //console.log(this.intersectionCube.position)
       } else {
         this.intersectionCube.visibility = false;
       }
@@ -162,7 +253,7 @@ module.exports = class LocalAvatar {
       if(checkCollisionFun(direction)) return;
       avatar.move(direction.setLength(translationLength));
       updateGroundElevationFun();
-      console.log('z');
+      //onsole.log('z');
       return
     });
     //BACKWARD
@@ -171,25 +262,26 @@ module.exports = class LocalAvatar {
       if(checkCollisionFun(direction)) return;
       avatar.move(direction.setLength(translationLength));
       updateGroundElevationFun();
-      console.log('s');
+      //console.log('s');
     });
     //LEFT
     inputManager.addKeyInput('q', 'keydown', function () {
       const dt = localContext.getDt();
       avatar.rotate(new Shared.THREE.Vector3(0, 0, speedRotate * dt));
-      console.log('q');
+      //console.log('q');
     });
     //RIGHT
     inputManager.addKeyInput('d', 'keydown', function () {
       const dt = localContext.getDt();
       avatar.rotate(new Shared.THREE.Vector3(0, 0, -speedRotate * dt));
-      console.log('d');
+      //console.log('d');
     });
 
     //warp to saved location
     inputManager.addKeyInput('m', 'keydown', function () {
-      cube.position.set(5522.95180710312, -3322.608827644959, -110.02345057404978);
-      cube.updateMatrixWorld();
+      localAvatar.position.set(5522.95180710312, -3322.608827644959, -110.02345057404978);
+      //console.log("saved position: ", localAvatar.position.add(worldOrigin));
+      localAvatar.updateMatrixWorld();
     });
   }
 
--- assets/worldScripts/avatar.js
+++ assets/worldScripts/avatar.js
@@ -32,6 +32,32 @@ module.exports = class Avatar {
     });
 
     world.addGameObject(this.avatar, worldContext, world.getGameObject());
+
+    /*
+    const campusY = new Shared.GameObject({
+      name: 'campusY',
+      components: {
+        Render: { idModel: 'campus_Yael' },
+        LocalScript: {
+          idScripts: [],
+        },
+      },
+    });
+    //campusY.object3D.scale.multiply(new Shared.THREE.Vector3(-1, -1, 1));
+    campusY.object3D.rotateOnAxis(new Shared.THREE.Vector3(0, 0, 1), -1*Math.PI);
+    //campusY.object3D.position.add(-world.origin);
+    //campusY.object3D.position.add(new Shared.THREE.Vector3(50, 0, 0));
+    campusY.object3D.position.set(5522.95180710312, -3322.608827644959, -110.02345057404978);
+    
+    //campusY.object3D.position.add(new Shared.THREE.Vector3(-1843551.0294686693, -5174222.462082106, -300));
+    //campusY.object3D.position.add(new Shared.THREE.Vector3(-5174222.462082106, -1843551.0294686693, -300));
+    campusY.setOutdated(true);
+    // world.addGameObject(campusY, worldContext, world.getGameObject());
+    console.log("worldGO", world.getGameObject())
+    //world.addGameObject(campusY, worldContext, world.parent.parent);
+
+    //world.addGameObject(campusR, worldContext, world.getGameObject());
+    //*/
   }
 
   tick() {}
