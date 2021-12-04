af745510f40b579c3a3923a8f66d281b19d65774
Antoine Webanck <antoine.webanck@gmail.com>
2021-12-04 23:08:14 +0100

    Tracking objectives and displaying instructions.

--- assets/localScripts/avatar.js
+++ assets/localScripts/avatar.js
@@ -45,7 +45,8 @@ module.exports = class LocalAvatar {
 
     //raycaster for avoiding buildings collisions with avatar
     this.raycaster = new Shared.THREE.Raycaster();
-    this.translationSpeed = 0.03;
+    //this.translationSpeed = 0.03;
+    this.translationSpeed = 0.01;
     this.rotationSpeed = 0.0012;
     this.acceptableSlope = 0.7;
     this.avatarSize = 1.7;
@@ -53,42 +54,90 @@ module.exports = class LocalAvatar {
 
     this.traceSecondsInterval = 0.1;
     this.trace = [];
+
+    this.objectives = [
+      {
+        "id":"cafet'U",
+        "coordinates": new Shared.THREE.Vector3(5567.575164457585, -3256.6427702494893, -108.00143772201538),
+        "distanceThreshold": 2
+      },
+      {
+        "id":"amphi F1",
+        "coordinates": new Shared.THREE.Vector3(5650.173994000734, -3278.2185901795615, -105.06365650052463)
+      },
+      {
+        "id":"accueil de l'Université du campus",
+        "coordinates": new Shared.THREE.Vector3(5572.270994168237, -3332.921776014817, -109.72455618791247),
+        "distanceThreshold": 5
+      },
+      {
+        "id":"halle des sports B",
+        "coordinates": new Shared.THREE.Vector3(5804.805060583292, -3437.70027410573, -99.08255773098806),
+        "distanceThreshold": 7
+      },
+      {
+        "id":"resto'U",
+        "coordinates": new Shared.THREE.Vector3(5748.186775903313, -3521.249842989633, -106.90629936294067),
+        "distanceThreshold": 2
+      },
+      {
+        "id":"café Filtre",
+        "coordinates": new Shared.THREE.Vector3(5782.290437117068, -3560.5980054803413, -106.42284282285995),
+        "distanceThreshold": 2
+      },
+      {
+        "id":"Kiosque",
+        "coordinates": new Shared.THREE.Vector3(5702.917950964341, -3384.870627430719, -101.17011562107709),
+        "distanceThreshold": 3
+      },
+      {
+        "id":"amphi L2",
+        "coordinates": new Shared.THREE.Vector3(5714.873221125032, -3463.3328045019757, -107.96326898109473)
+      },
+      {
+        "id":"bâtiment B",
+        "coordinates": new Shared.THREE.Vector3(5617.592245786231, -3248.1625083272656, -105.3557973487854),
+        "distanceThreshold": 2
+      },
+      {
+        "id":"forum",
+        "coordinates": new Shared.THREE.Vector3(5638.039973720276, -3320.6375219867145, -105.32150721865845),
+        "distanceThreshold": 7
+      },
+    ];
+    this.currentObjective = 0;
+
+    this.warped = false;
   }
 
-  addTileLayerToArray(array, tilesManager, layerName) {
-    let layerManager = null;
-    //console.log(tilesManager);
-    for (let index = 0; index < tilesManager.length; index++) {
-      const element = tilesManager[index];
-      if (element.layer.id == layerName) {
-        layerManager = element;
-        break;
-      }
-    }
-  
-    if (!layerManager) throw new Error('no layer called ', layerName);
-  
-    layerManager.tiles.forEach(function (t) {
-      const obj = t.getObject3D();
-      if (obj) array.push(obj);
-    });
-  }
+  //addTileLayerToArray(array, tilesManager, layerName) {
+  //  let layerManager = null;
+  //  //console.log(tilesManager);
+  //  for (let index = 0; index < tilesManager.length; index++) {
+  //    const element = tilesManager[index];
+  //    if (element.layer.id == layerName) {
+  //      layerManager = element;
+  //      break;
+  //    }
+  //  }
+  //
+  //  if (!layerManager) throw new Error('no layer called ', layerName);
+  //
+  //  layerManager.tiles.forEach(function (t) {
+  //    const obj = t.getObject3D();
+  //    if (obj) array.push(obj);
+  //  });
+  //}
   buildingsHit(tilesManager, origin, direction) {
     // const gV = localContext.getGameView();
-
     // const tilesManager = gV.getLayerManager().tilesManagers;
     const buildings = [this.campus];
     // this.addTileLayerToArray(buildings, tilesManager, '3d-tiles-layer-building');
 
-    // const pos = this.avatar.getPosition();
-    // const ref = localContext.getGameView().getObject3D().position;
-
-    // this.raycaster.ray.origin = pos.add(ref);
     this.raycaster.ray.origin = origin;
     this.raycaster.ray.direction = direction;
     //console.log(origin);
 
-    
     const intersections = this.raycaster.intersectObjects(buildings, true);
     if(intersections.length) return intersections[0];
     return null;
@@ -144,7 +193,7 @@ module.exports = class LocalAvatar {
     this.mapsIdsCoordinates.forEach(function (mapIdCoordinates) {
       const id = mapIdCoordinates.id;
       const p = mapIdCoordinates.coordinates;
-      const o3D = this.map.object3D.getObjectByName(id);
+      const o3D = mapO3D.getObjectByName(id);
       if(p.distanceTo(position) < this.mapsDistanceThreshold) {
         mapNearby = true;
         o3D.visible = true;
@@ -155,17 +204,60 @@ module.exports = class LocalAvatar {
   }
 
   traceMovement() {
+    //Trace only after warp and before completion of objectives.
+    if(!this.warped || this.currentObjective >= this.objectives.length) return;
+
     //Trace position.
     const time = Date.now();
     if(this.trace.length == 0 || (time - this.trace[this.trace.length - 1].time) > this.traceSecondsInterval * 1000)
-    this.trace.push({
-      "time": time,
-      "matrix": this.localAvatar.matrix
-    });
-    //console.log(time/1000 % 100);
-    //console.log(this.trace.length);
-    //console.log(this.localAvatar);
+    {
+      this.trace.push({
+        "time": time,
+        "matrix": this.localAvatar.matrix
+      });
+      //console.log(time/1000 % 100);
+      //console.log(this.trace.length);
+      //console.log(this.localAvatar);
+    }
   }
+
+
+  updateLabel() {
+    if(!this.warped) {
+      this.label.innerHTML = "Waiting for warp (M key) to give objectives.";
+      return;
+    }
+
+    if(this.currentObjective >= this.objectives.length)
+    {
+      const startTime = this.trace[0].time;
+      const finishTime = this.trace[this.trace.length - 1].time;
+      const duration = finishTime - startTime;
+      this.label.innerHTML = 'All objectives completed in ' + duration/1000 + " seconds!";
+      this.label.innerHTML += "<br>";
+      this.label.innerHTML += "Please press key T to save trace.";
+      return;
+    }
+    this.label.innerHTML = 'Current objective: ' + this.objectives[this.currentObjective].id;
+  }
+
+  checkForNearbyObjective() {
+    if(this.currentObjective >= this.objectives.length) return;
+
+    const position = this.localAvatar.position;
+    const objective = this.objectives[this.currentObjective];
+    const threshold = (objective.distanceThreshold ? objective.distanceThreshold : 1);
+    if(objective.coordinates.distanceTo(position) < threshold) {
+      console.log("Objective '" + objective.id + "' reached!");
+      this.currentObjective++;
+
+      this.updateLabel();
+
+      if(this.currentObjective >= this.objectives.length) return;
+      console.log("New objective is '" + this.objectives[this.currentObjective].id + "'");
+    }
+  }
+
 
   init() {
     const avatar = arguments[0].computeRoot().findByName('avatar');
@@ -175,33 +267,18 @@ module.exports = class LocalAvatar {
     const tilesManager = gV.getLayerManager().tilesManagers;
     const worldOrigin = gV.getObject3D().position;
 
-    //Input manager of the game
-    const inputManager = localContext.getGameView().getInputManager();
-
-    //const thisLocalAvatar = this;
-
-
-
-    //local avatar/cube
+    //local avatar
     const scene = gV.getScene();
-    //console.log(avatar.object3D);
-    // const geometry = new Shared.THREE.BoxGeometry(3, 3, 3);
-    // const material = new Shared.THREE.MeshBasicMaterial( {color: 0xff0000} );
-    // const cube = new Shared.THREE.Mesh( geometry, material );
-    const localAvatar = avatar.object3D;//.clone();
-    //avatar.object3D.visible = false;
+    const localAvatar = avatar.object3D;
     this.localAvatar = localAvatar;
     //scene.add(localAvatar);
     //console.log(scene);
 
-    //intersection cube
+    //debug intersection cube
     const geometry = new Shared.THREE.BoxGeometry( 1, 1, 1 );
     const material = new Shared.THREE.MeshBasicMaterial( {color: 0x00ff00} );
     this.intersectionCube = new Shared.THREE.Mesh( geometry, material );
     scene.add( this.intersectionCube );
-
-    //console.log(this.localAvatar);
-    //console.log(scene);
 
     //referential debug gizmo
     if(this.debugGizmo) {
@@ -228,12 +305,7 @@ module.exports = class LocalAvatar {
     }
 
 
-
-
-
-
-
-
+    //Manual instanciation of campus.
     this.campus = gV.assetsManager.createRenderData('campus_Yael').object;
     //console.log(this.campus);
     this.campus.rotateOnAxis(new Shared.THREE.Vector3(0, 0, 1), -0.5*Math.PI); //here or manually on -y axis in rotation parameter of local_game_config.json
@@ -241,11 +313,19 @@ module.exports = class LocalAvatar {
     //console.log("campus", this.campus);
     scene.add(this.campus);
 
-    //Campus map is initilized as invisible.
+    //Campus map is initialized as invisible.
     this.map = arguments[0].computeRoot().findByName('map');
     //console.log(this.map);
     this.map.object3D.visible = false;
 
+    //Label div to print infos on objectives.
+    this.label = document.createElement('div');
+    localContext.getGameView().appendToUI(this.label);
+    this.updateLabel();
+
+
+    //Input manager of the game
+    const inputManager = localContext.getGameView().getInputManager();
     const dt = localContext.getDt();
     const translationLength = this.translationSpeed * dt;
     const rotationAngle = this.rotationSpeed * dt;
@@ -303,9 +383,11 @@ module.exports = class LocalAvatar {
 
       //Trace position.
       this.traceMovement();
+
+      //Check for objective completion.
+      this.checkForNearbyObjective();
     }.bind(this);
 
-    
     //FORWARD
     inputManager.addKeyCommand('move_forward', ['z'], function () {
       const direction = avatar.computeForwardVector();
@@ -345,13 +427,17 @@ module.exports = class LocalAvatar {
     });
 
     //warp to saved location
-    const trace = this.trace;
     inputManager.addKeyInput('m', 'keydown', function () {
+      //Allow only initial warp.
+      if(this.warped) return;
+      this.warped = true;
+
       localAvatar.position.set(5522.95180710312, -3322.608827644959, -110.02345057404978);
       //console.log("saved position: ", localAvatar.position.add(worldOrigin));
       localAvatar.updateMatrixWorld();
-      trace.length = 0;
-    });
+
+      this.updateLabel();
+    }.bind(this));
 
     //save trace.
     function download(content, fileName, contentType) {
@@ -366,8 +452,8 @@ module.exports = class LocalAvatar {
       //const dateString = new Date().toLocaleString("fr-FR");
       const dateString = Date.now();
       //console.log(dateString);
-      download(JSON.stringify(trace), 'trace-' + dateString + '.txt', 'text/plain');
-    });
+      download(JSON.stringify(this.trace), 'trace-' + dateString + '.txt', 'text/plain');
+    }.bind(this));
   }
 
   tick() {}
